69e9317337e75b3ad2e6772e1278536e8db2790f
Fabio-Lucas-07 <fabiolucaslima615@gmail.com>
2025-10-16 02:36:36 -0300

    Inserction of the tailwindCSS

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import './style.css'
 import Logo from '../../../public/assets/logo.png'
 import Menu from '../../../public/assets/menu.png'
+import Fgts from '../../../public/assets/fgts.png'
 
 function Home() {
 
@@ -8,25 +9,30 @@ function Home() {
 
   return (
 
-    <div>
-      <div className='top-bar'>
+    <div className='body bg-[#034C8C] m-0 '>
+      <div className = 'flex flex-row items-center text-[#FFFFFF] bg-[#013884] shadow-xl/20 fixed w-full'>
 
-        <img src={Logo} />
-        <div className='navigation'>
-          <a href="">Quem Somos</a>
-          <a href="">Produtos</a>
-          <a href="">Serviços</a>
-          <a href="">Contato</a>
+        <img src={Logo} className='ml-10 mr-10' />
+        <div className= "nav" >
+          <a href="" className="nav-itens ">Quem Somos</a>
+          <a href="" className="nav-itens">Produtos</a>
+          <a href="" className="nav-itens">Serviços</a>
+          <a href="" className="nav-itens">Contato</a>
         </div>
-        <button type='button'>Consultar</button>
+        <button type='button' className = 'rounded-full bg-[#058605] cursor-pointer ml-auto h-9 w-30 transition duration-500 hover:bg-[#035703] ml-10 mr-10 shadow-xl/25' >Consultar</button>
       </div>
 
-      <div className='carrosel'>
-
+      <div className='mother flex flex-col gap-100'>
+        <div className='carrosel '>
+        </div>
+        <div className='carrosel'>
+        </div>
+        <div className='carrosel'>
+        </div> 
       </div>
 
     </div>
-    
+
 
 
   )
